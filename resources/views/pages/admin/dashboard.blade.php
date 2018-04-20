3df63749949d34753e6e24897911f3e51764afa0
Unknown <emilyyou95@gmail.com>
2018-04-19 23:35:51 -0400

    Updated margin-bottom property for task boxes on admin dashboard

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -3,12 +3,11 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-
         <div class="col-md-8 col-md-offset-2 text-center">
             <h2 style="margin-top: 40px; margin-bottom: 100px;">Hi! What would you like to do today?</h2>
 
-            <div class="row" style="margin-bottom: 70px;">
-                <div class="col-md-3">
+            <div class="row">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/update-hours">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-clock" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
@@ -16,7 +15,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/update-services">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-asterisk" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
@@ -24,7 +23,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/update-products">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-barcode" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
@@ -32,7 +31,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/show-enquiries">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-comment-dots" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
@@ -43,7 +42,7 @@
             </div>{{--row--}}
 
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/add-promotion">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-certificate" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
@@ -51,7 +50,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/update-services">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-id-card" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
@@ -59,7 +58,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/update-products">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-gift" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
@@ -67,7 +66,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3" style="margin-bottom: 15px;">
                     <a class="taskbox" style="cursor: pointer;" href="/show-enquiries">
                         <div style="height: 250px; background-color: #f5f8fa; border: 1px #e0e4e6 solid;">
                             <i class="fas fa-calendar-alt" style="font-size: 100px; width: 100%; margin-top: 50px; margin-bottom: 25px;"></i>
